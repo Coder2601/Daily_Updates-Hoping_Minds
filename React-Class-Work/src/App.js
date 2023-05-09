c8c4ac5289238c93db4114fc58c62a97626d0c20
Divya Goyal <77169739+Coder2601@users.noreply.github.com>
2023-05-09 10:42:45 +0530

    Add files via upload

--- React-Class-Work/src/App.js
+++ React-Class-Work/src/App.js
@@ -1,19 +1,49 @@
-import Courses from './Components/class-based-comps/Courses';
-import {Items, Navigation, List } from './Components/class-based-comps/Navigation';
-import Form from './Components/class-based-comps/formFile'
-import Counter from './Components/class-based-comps/states/Counter';
+// import Courses from './Components/class-based-comps/Courses';
+// import {Items, Navigation, List } from './Components/class-based-comps/Navigation';
+// import Parent from './Components/class-based-comps/conditional-rendering/Parent';
+// import Form from './Components/class-based-comps/form-handling/formFile'
+// import Cycle from './Components/class-based-comps/lifecycleMethods&PureComponents/Cycle';
+// import ControlButtons from './Components/class-based-comps/props/ControlButtons';
+// import ParentForm from './Components/class-based-comps/props/ParentForm';
+// import Calculator from './Components/class-based-comps/states/Calculator';
+// import Counter from './Components/class-based-comps/states/Counter';
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//     {/* <Form /> */}
+//     {/* <Items />
+//     <Courses />
+//     <List />
+//     <Navigation /> */}
+//     {/* <Counter /> */}
+//     {/* <Calculator /> */}
+//     {/* <Form /> */}
+//     {/* <Parent /> */}
+//     {/* <ControlButtons /> */}
+//     {/* <ParentForm /> */}
+//     {/* <Cycle /> */}
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from 'react'
+import Counter from './Components/function-based-comps/useStateHook/Counter'
+import Buttons from './Components/function-based-comps/props/Buttons'
+import ParentForm from './Components/function-based-comps/props/Form/ParentForm'
+
+const App = () => {
   return (
-    <div>
-    {/* <Form /> */}
-    {/* <Items />
-    <Courses />
-    <List />
-    <Navigation /> */}
-    <Counter />
-    </div>
-  );
+    <>
+      {/* <Counter /> */}
+      {/* <Buttons /> */}
+      <ParentForm />
+    </>
+  )
 }
 
-export default App;
+export default App
