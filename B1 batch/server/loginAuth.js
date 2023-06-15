f0bd60da5c4cabad40a9f6b65b911f4b03b2cadc
Divya Goyal <77169739+Coder2601@users.noreply.github.com>
2023-06-15 11:18:17 +0530

    Add files via upload

---
+++ B1 batch/server/loginAuth.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const routes = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+let users = [{
+    id: "1",
+    name: "Adi",
+    password: "$2b$10$FSaQkz9sVtAAQiZbGQMIWu3r9OKIgFlXP5QfbPLySVA8VavRcLXGm"
+  }];
+
+routes.post('/register', async (req, res) => {
+    let id = users.length + 1;
+    try {
+        let encPass = await bcrypt.hash(req.body.password, 10);
+        let newUser = {
+            id: id,
+            name: req.body.name,
+            password: encPass
+        }
+        users.push(newUser)
+        res.send({ msg: 'Registered Successfully', users: users })
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+routes.post('/login', async(req, res) => {
+    let id = req.body.id;
+    let pass = req.body.password;
+
+    const user = users.find(user=>user.id === id)
+    // console.log(user);
+    if(user!== undefined){
+        const result = await bcrypt.compare(pass,user.password);
+        console.log(result);
+        if(result){
+            res.send('Login success');
+            const token = jwt.sign(user,process.env.Security_Key);
+            console.log(token);
+
+        }else{
+            res.send('Login Falied')
+        }
+    }else{
+        res.send('User not found');
+    }
+    // res.send('Looking for user')
+})
+
+module.exports = routes
--- B1 batch/server/server.js
+++ B1 batch/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+
 const server = express();
 const PORT = 8000;
 const clientRoute = require('./Routes/userRoutes');
