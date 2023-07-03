1015fbbfb7433e3c326eb71d384c0f220db3cb84
Divya Goyal <77169739+Coder2601@users.noreply.github.com>
2023-07-03 14:10:46 +0530

    Add files via upload

---
+++ A4/server/server.js
@@ -0,0 +1,25 @@
+//create a server using express and node.js
+
+//it receive HTTP requests (GET, POST, PUT, PATCH, DELETE) and send responses(data-(string, JSON), status code-100(information),200(Success),300(Redirectional),400(FED Error),500(BED Error)) in return
+
+//all requests are sent to perform CRUD(Create, Read, Update, Delete) operation 
+//to run the server we can use "node", "nodemon"
+
+
+const express = require('express');
+const PORT = 8000;
+//65535- maximum limit of port number
+//avoid using 3000 series of port number as FED runs on it.
+
+const server = express();
+//express function returns an object(server) that is used to receive requests and send reesponses
+
+server.post('/',(request, response)=>{
+    response.send("POST request at root received").status(204);
+})
+server.get('/',(request, response)=>{
+    response.send("GET request at root received").status(200);
+})
+
+
+server.listen(PORT,()=>console.log(`Server is running at port: ${PORT}`))
